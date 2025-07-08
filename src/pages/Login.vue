974390bae2e7fa0d54867a19892d9984469eb743
maikon.muniz <maikon.muniz@yahoo.com.br>
2025-07-08 15:39:29 -0300

    feat: create implemention login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,3 +1,46 @@
 <template>
-    <h1>Teste login</h1>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="handleLogin">
+      <label>
+        Username:
+        <input v-model="username" type="text" required />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input v-model="password" type="password" required />
+      </label>
+      <br />
+      <button type="submit">Login</button>
+    </form>
+    <p v-if="error" style="color: red;">{{ error }}</p>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import api from '@/services/api'
+import { useRouter } from 'vue-router'
+
+const username = ref('')
+const password = ref('')
+const error = ref('')
+const router = useRouter()
+
+const handleLogin = async () => {
+  try {
+    const response = await api.post('account/login', {
+      username: username.value,
+      password: password.value
+    })
+    const token = response.data.token
+    localStorage.setItem('token', token)
+    router.push('/about')
+  } catch (err) {
+    console.log(username.value)
+    error.value = 'Credenciais inválidas ou erro no servidor'
+    console.error(err)
+  }
+}
+</script>
